4bfab5c96263704434af35d1e696ff8fabafee48
fatoh-dev <fatohnasr360@gmail.com>
2025-06-09 10:57:56 +0300

    add and store bookmarks and display

--- js/main.js
+++ js/main.js
@@ -3,3 +3,42 @@
 function selectElement(element) {
   return document.querySelector(element);
 }
+var bookmarks = [];
+
+if (localStorage.getItem("bookmarks")) {
+  displayBookMark();
+}
+
+function addBookMark() {
+  var bookmark = {
+    name: selectElement(".b-input").value,
+    url: selectElement(".u-input").value,
+  };
+
+  bookmarks.push(bookmark);
+
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  displayBookMark();
+  clearInputs();
+}
+
+function displayBookMark() {
+  var marks = JSON.parse(localStorage.getItem("bookmarks"));
+  var html = "";
+  for (var i = 0; i <= marks.length - 1; i++) {
+    html += `
+         <tr>
+            <td>${i}</td>
+            <td>${marks[i].name}</td>
+            <td><a href="${marks[i].url}">Visit</a></td>
+            <td><button>delete</button></td>
+          </tr>`;
+  }
+  selectElement(".book-list").innerHTML = html;
+}
+
+function clearInputs() {
+  selectElement(".b-input").value = "";
+  selectElement(".u-input").value = "";
+}
+document.querySelector(".btn").addEventListener("click", addBookMark);
